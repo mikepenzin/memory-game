ef0ab4c632ecf8f39d57927f731381c0b42e3691
Mike Penzin <mike.penzin@gmail.com>
2018-09-19 00:17:27 -0700

    added animation to flips

--- views/main/main-page-directive.js
+++ views/main/main-page-directive.js
@@ -2,29 +2,41 @@
 
 var memoryGameMainView = angular.module("memoryGameMainView", []);
 
-memoryGameMainView.directive("memoryGameMainView", ['$window', function($window) {
-    var controller = function ($scope) {
+memoryGameMainView.directive("memoryGameMainView", ['$window', '$timeout', function($window, $timeout) {
+    var controller = function($scope) {
 
         var ctrl = this;
-        
-        ctrl.GameStarted = function () {
+
+        ctrl.GameStarted = function() {
             $scope.isGameStarted = true;
         }
-    };   
-    
+    };
+
     return {
         restrict: 'AE',
-        scope : false,
+        scope: false,
         controller: controller,
         controllerAs: 'ctrl',
         bindToController: true,
-        templateUrl: 'views/main/main-page.html',        
+        templateUrl: 'views/main/main-page.html',
         link: function(scope, element, attrs) {
-            
-            scope.$on('restart', function (event, data){
+
+            // scope.$on('', function(event, data) {
+            //     console.log("HERE!!!");
+            //     debugger;
+            // });
+
+            $timeout(hello, 0);
+
+            function hello() {
+                console.log('HELLO!!!', element);
+                $(element).fadeIn(3000);
+            }
+
+            scope.$on('restart', function(event, data) {
                 $window.location.reload();
             });
-            
+
         }
     };
 
